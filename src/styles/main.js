0f934e4b8868e842b2e2ebf6d07bdb716904b7e6
Julio de oliveira <jun_io2@hotmail.com>
2024-09-09 21:46:15 -0300

    ajuste no mobile

--- src/styles/main.js
+++ src/styles/main.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]')
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual < alturaHero) {
+            ocultaElementoDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+
+    // seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
@@ -11,7 +27,20 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+
+    //seção FAQ, accordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFecharesposta)
+    }
 })
+
+function abreOuFecharesposta(elemento) {
+    const classe ='faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe)
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -19,6 +48,17 @@ function removeBotaoAtivo() {
     for(let i = 0; i <buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active');
     }
+}
+
+
+function ocultaElementoDoHeader () {
+    const header = document.querySelector('header');
+    header.classList.add("header--is-hidden");
+}
+
+function exibeElementosDoHeader () {
+    const header = document.querySelector('header');
+    header.classList.remove("header--is-hidden");
 }
 
 function escondeTodasAbas() {
